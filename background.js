b9af2e6592ef6a414615e031d5a0e2fcf73b5ab4
tzwm <wangjunzhe4444@gmail.com>
2014-01-14 00:00:54 +0800

    Got friends list by api and added avatar to subject-friends-comments.

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+var friends_list;
+
+function getFriendsList(uid){
+  var url = 'http://api.douban.com/shuo/v2/users/' +
+            uid + '/following?count=200';
+
+  $.get(url, function(data, status){
+    if(status=='success'){
+      friends_list = data;
+
+      var i = 0;
+      while(i<friends_list.length){
+        if('original_site_id' in friends_list[i])
+          friends_list.splice(i, 1);
+        else
+          i++;
+      }
+    }
+  });
+}
+
 function getDomainFromUrl(url){
   var host = "null";
   if(typeof url == "undefined" || null == url)
@@ -17,7 +38,15 @@ function isDoubanMovieUrl(url){
 function checkForValidUrl(tabId, changeInfo, tab) {
   if(isDoubanMovieUrl(tab.url)){
     chrome.pageAction.show(tabId);
-  }
+
+    getFriendsList('tzwm');
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "test", data: friends_list}, function(response) {
+        console.log(response.farewell);
+      });
+    });
+  };
 };
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
--- movie_subject_content.js
+++ movie_subject_content.js
@@ -3,7 +3,7 @@ function removeAd(){
   $("#dale_movie_subject_middle_right").remove();
 }
 
-function addFriendComment(){
+function addFriendsCommentsSubject(){
   //var others_interests = $("#subject-others-interests");
   //var friends_comments = $.extend(true, {}, others_interests); 
 
@@ -13,6 +13,36 @@ function addFriendComment(){
   friends_comments.prepend("<h2>好友评价 · · · · · ·</h2>");
 }
 
+function listFriendsComments(friends_list){
+  var friends_comments = $("#subject-friends-comments");
+  friends_comments.append('<ul></ul>');
+  var ul = friends_comments.children('ul');
+  
+  for(var i=0; i<4; i++){
+    var li = '<li>';
+    var avatar = '<a class=\"others-interest-avatar\" href=\"';
+    avatar += friends_list[i].url + '\">';
+    avatar += '<img class=\"pil\" src=\"' + 
+      friends_list[i].small_avatar + '\"></a>';
+
+    li += avatar + '</li>';
+
+    ul.append(li);
+  }
+}
+
 removeAd();
-addFriendComment();
+addFriendsCommentsSubject();
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse){
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if(request.action == 'test'){
+      listFriendsComments(request.data);
+      sendResponse({farewell: 'goodbye'});
+    }
+  });
+
 
